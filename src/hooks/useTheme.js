678b6fd34f0a039b7e76a75dc165c34e253c43b9
Luis Hernández <ldhdv95@gmail.com>
2019-07-13 09:24:01 -0500

    Update useTheme hook: Fix localstorage not defined error

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,32 +1,34 @@
 import { useEffect, useState } from "react"
 
 export default () => {
-  const data = localStorage.getItem("theme") || "light"
-  const [theme, setTheme] = useState(data)
-  const toggleTheme = () => {
-    if (theme !== "dark") {
-      localStorage.setItem("theme", "dark")
-      document.documentElement.style.backgroundColor = "#000"
-      setTheme("dark")
-    } else {
-      localStorage.setItem("theme", "light")
-      document.documentElement.style.backgroundColor = "#fff"
-      setTheme("light")
+  if (typeof window !== "undefined") {
+    const data = window.localStorage.getItem("theme") || "light"
+    const [theme, setTheme] = useState(data)
+    const toggleTheme = () => {
+      if (theme !== "dark") {
+        window.localStorage.setItem("theme", "dark")
+        document.documentElement.style.backgroundColor = "#000"
+        setTheme("dark")
+      } else {
+        window.localStorage.setItem("theme", "light")
+        document.documentElement.style.backgroundColor = "#fff"
+        setTheme("light")
+      }
     }
-  }
 
-  useEffect(() => {
-    const localTheme = localStorage.getItem("theme")
-    if (localTheme) {
-      localTheme === "light"
-        ? (document.documentElement.style.backgroundColor = "#fff")
-        : (document.documentElement.style.backgroundColor = "#000")
-      setTheme(localTheme)
+    useEffect(() => {
+      const localTheme = window.localStorage.getItem("theme")
+      if (localTheme) {
+        localTheme === "light"
+          ? (document.documentElement.style.backgroundColor = "#fff")
+          : (document.documentElement.style.backgroundColor = "#000")
+        setTheme(localTheme)
+      }
+    }, [theme])
+
+    return {
+      theme,
+      toggleTheme,
     }
-  }, [theme])
-
-  return {
-    theme,
-    toggleTheme,
   }
 }
