1eb6c9893527418f35141767f1505112af7778e6
Luis Hernandez <lhernandez@fullstacklabs.co>
2020-02-15 17:07:45 -0500

    Remove persistency on localStorage

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,37 +1,19 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default () => {
-  if (typeof window !== "undefined") {
-    const data = window.localStorage.getItem("theme") || "light"
-    const [theme, setTheme] = useState(data)
-    const toggleTheme = () => {
-      if (theme !== "dark") {
-        window.localStorage.setItem("theme", "dark")
-        document.documentElement.style.backgroundColor = "#000"
-        setTheme("dark")
-      } else {
-        window.localStorage.setItem("theme", "light")
-        document.documentElement.style.backgroundColor = "#fff"
-        setTheme("light")
-      }
+  const [theme, setTheme] = useState("light")
+  const toggleTheme = () => {
+    if (theme !== "dark") {
+      document.documentElement.style.backgroundColor = "#000"
+      setTheme("dark")
+    } else {
+      document.documentElement.style.backgroundColor = "#fff"
+      setTheme("light")
     }
+  }
 
-    useEffect(() => {
-      const localTheme = window.localStorage.getItem("theme") || 'light'
-      localTheme === "light"
-        ? (document.documentElement.style.backgroundColor = "#fff")
-        : (document.documentElement.style.backgroundColor = "#000")
-      setTheme(localTheme)
-    }, [theme])
-
-    return {
-      theme,
-      toggleTheme,
-    }
-  } else {
-    return {
-      theme: "light",
-      toggleTheme: () => {},
-    }
+  return {
+    theme,
+    toggleTheme,
   }
 }
